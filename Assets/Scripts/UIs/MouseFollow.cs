d69e403bba0691959470a342a2d666caaf33c16a
kangtoe <kangtoe99@gmail.com>
2024-10-16 16:45:20 +0900

    크로스헤어 스프라이트 임포트, 마우스 위치를 따라다니는 UI 스크립트 작성

---
+++ Assets/Scripts/UIs/MouseFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+{
+    RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {            
+        rectTransform.position = Input.mousePosition;
+        Cursor.visible = false;
+    }
+}
