50af65fee1467016aee7cdbf6419fab145156e3b
kangtoe <kangtoe99@gmail.com>
2024-10-19 22:25:32 +0900

    조준선 간격을 조절 가능한 Crosshair, UiManager 스크립트 작성

---
+++ Assets/Scripts/UIs/Crosshair.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    //public Transform ui;
+    //public Transform rotator;
+
+    public float speadIn = 1f;    
+    
+    public float spreadMin = 0;
+    public float spreadMax = 20;    
+
+    [SerializeField]
+    float currSpead = 0f;
+
+    [Header("crosshair UI")]
+    public RectTransform arr_ur;
+    public RectTransform arr_ul;
+    public RectTransform arr_dr;
+    public RectTransform arr_dl;
+
+    void Start()
+    {
+        //Cursor.visible = false;
+        //RotateCross(3);
+    }
+
+    void Update()
+    {        
+        AdjustSpread(speadIn * Time.deltaTime * -1);
+    }
+
+    // ui 초기화. 무기 교체 시 호출
+    public void Init()
+    {
+        //Debug.Log("Init");        
+        //rotator.rotation = Quaternion.Euler(0, 0, 0);
+        //currentSpead = 0;
+        StopAllCoroutines();
+    }
+
+    // weapon에서 호출
+    public void AdjustSpread(float t)
+    {
+        currSpead += t;
+        currSpead = Mathf.Clamp(currSpead, spreadMin, spreadMax);
+        SetArrSpread(currSpead);
+    }
+
+    void SetArrSpread(float f)
+    {
+        arr_ur.anchoredPosition = new Vector2(f, f);
+        arr_ul.anchoredPosition = new Vector2(-f, f);
+        arr_dr.anchoredPosition = new Vector2(f, -f);
+        arr_dl.anchoredPosition = new Vector2(-f, -f);
+    }
+
+    // time 동안 360도 회전한다
+    //public void RotateCross(float time)
+    //{
+    //    StartCoroutine(RotateCrossCr(time));
+    //}
+
+    //IEnumerator RotateCrossCr(float time)
+    //{
+    //    // 프레임 당 회전할 값 구하기
+    //    float frameDeg = 360 / time;
+
+    //    // 매 프레임마다 회전
+    //    for (; time > 0; time -= Time.deltaTime)
+    //    {
+    //        rotator.Rotate(0, 0, frameDeg * Time.deltaTime);
+    //        yield return new WaitForEndOfFrame();
+    //    }
+
+    //    rotator.rotation = Quaternion.Euler(0, 0, 360);
+    //}
+}
---
+++ Assets/Scripts/UIs/UiManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiManager : MonoSingleton<UiManager>
+{
+    Crosshair Crosshair 
+    {
+        get {
+            if (!crosshair) crosshair = FindObjectOfType<Crosshair>();
+            return crosshair;
+        }
+    }
+    Crosshair crosshair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Crosshair.AdjustSpread(5);
+            Debug.Log("GetMouseButtonDown");
+        }
+    }
+}
