b16752fded5d6a1132293c07e1fd14fdc7d31a90
kangtoe <kangtoe99@gmail.com>
2024-10-22 00:15:53 +0900

    현재 방향과 관련없이 target으로 이동하는 MoveToTarget 스크립트 작성

---
+++ Assets/Scripts/Movements/MoveToTarget.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 현재 방향과 관련없이 target으로 이동하도록 힘을 가한다 
+public class MoveToTarget : MonoBehaviour
+{
+    public float movePower = 1f;
+    Rigidbody2D rbody;
+
+    FindTarget findTarget;
+    Transform Target => findTarget?.Target;
+
+    void Start()
+    {
+        findTarget = GetComponent<FindTarget>();
+        rbody = GetComponent<Rigidbody2D>();        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 dir = transform.up;
+        if (Target) dir = (Target.position - transform.position).normalized;
+
+        Debug.Log(dir);
+        rbody.AddForce(dir * movePower * rbody.mass);
+    }
+}
