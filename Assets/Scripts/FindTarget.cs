80ec7f82a08c10d1a5d9077b967dbee792f641fe
kangtoe <kangtoe99@gmail.com>
2024-10-21 09:20:08 +0900

    레어어, 영역 및 거리 등에 따라 오브젝트를 탐색하는 스크립트 작성

---
+++ Assets/Scripts/FindTarget.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindTarget : MonoBehaviour
+{
+    [SerializeField] LayerMask targetLayer;
+    [SerializeField] float searchRaduius = 20;    
+    public bool bFindTargetEvenThereIs = false;
+    
+    [Header("for debug")]
+    [SerializeField] Transform target;
+    public Transform Target => target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(UpdateTargetCr());
+    }
+
+    IEnumerator UpdateTargetCr()
+    {
+        while (true)
+        {            
+            if(!target || bFindTargetEvenThereIs)
+            {
+                // 타겟 갱신
+                Collider2D coll = GetClosestCollider(transform.position, searchRaduius, targetLayer);
+                if (coll) target = coll.attachedRigidbody.transform;
+            }            
+
+            // 대기
+            yield return new WaitForSeconds(1);
+        }        
+    }
+
+    Collider2D GetClosestCollider(Vector2 pos, float radius, LayerMask layer)
+    {                
+        Collider2D[] colls = Physics2D.OverlapCircleAll(pos, radius, layer);
+        Collider2D closestColl = null;
+        float closestDist = Mathf.Infinity;
+
+        foreach (Collider2D coll in colls)
+        {            
+            // 플레이어 또는 기준 지점과 각 오브젝트 간의 거리를 계산
+            float distance = Vector3.Distance(transform.position, pos);
+
+            // 현재까지 가장 가까운 오브젝트보다 더 가까운 오브젝트를 찾으면 업데이트
+            if (distance < closestDist)
+            {
+                closestDist = distance;
+                closestColl = coll;
+            }
+        }
+        return closestColl;        
+    }
+
+}
