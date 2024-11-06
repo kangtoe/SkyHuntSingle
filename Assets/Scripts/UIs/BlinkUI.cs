1ce03de2931bc9715c7016424cf07e4991a52cb1
kangtoe <kangtoe99@gmail.com>
2024-11-06 10:19:58 +0900

    점멸 효과 UI 스크립트 추가 및 적용

---
+++ Assets/Scripts/UIs/BlinkUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BlinkUI : MonoBehaviour
+{
+    [SerializeField] Graphic ui;
+	[SerializeField] float offInterval = 0.5f;
+	[SerializeField] float onInterval = 0.5f;
+
+	void Start()
+	{
+		ui = GetComponent<Text>();
+		StartCoroutine(BlinkText());
+	}
+
+	public IEnumerator BlinkText()
+	{
+		while (true)
+		{			
+			ui.color = new Color(ui.color.r, ui.color.g, ui.color.b, 0);
+			yield return new WaitForSeconds(offInterval);
+			ui.color = new Color(ui.color.r, ui.color.g, ui.color.b, 1);
+			yield return new WaitForSeconds(onInterval);
+		}
+	}
+}
