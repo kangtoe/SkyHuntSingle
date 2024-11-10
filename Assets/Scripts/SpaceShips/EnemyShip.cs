2e7271ad56ebba778822a19edbc71994c485462b
kangtoe <kangtoe99@gmail.com>
2024-11-10 10:31:05 +0900

    적 격파 시, 획득 점수 UI 표기

--- Assets/Scripts/SpaceShips/EnemyShip.cs
+++ Assets/Scripts/SpaceShips/EnemyShip.cs
@@ -8,12 +8,16 @@ public class EnemyShip : MonoBehaviour
     public int point = 100;
     //public int spwanMinWave = 0; // 이 이후의 wave에만 등장 가능
 
+    [Multiline]
+    public string desc;
+
     private void Start()
     {
         Damageable damageable = GetComponent<Damageable>();
 
         damageable.onDead.AddListener(delegate
-        {                        
+        {
+            UiManager.Instance.CreateText("+" + point, transform.position);
             ScoreManager.Instance.AddScore(point);
             LevelManager.Instance.GetExp(point);
         });
