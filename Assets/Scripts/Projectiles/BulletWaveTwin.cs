5398fd3e17ccceddd40f1c2a69e3dd60ae593f30
kangtoe <kangtoe99@gmail.com>
2024-11-11 00:37:15 +0900

    BulletWave 클래스 기능을 MoveWave, BulletWaveTwin 두 클래스로 분리

---
+++ Assets/Scripts/Projectiles/BulletWaveTwin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MoveWave))]
+public class BulletWaveTwin : BulletBase
+{
+    [HideInInspector] public bool bSpawnTwin = true;
+    
+    MoveWave moveWave;
+    MoveWave MoveWave {
+        get {
+            if(!moveWave) moveWave = GetComponent<MoveWave>();
+            return moveWave;
+        }
+    }
+
+    override protected void Start()
+    {
+        base.Start();
+
+        rbody.velocity = Vector2.zero;
+        MoveWave.movePower = movePower;
+
+        // twin 생성
+        if (bSpawnTwin)
+        {
+            BulletWaveTwin twin = Instantiate(this);
+            twin.Init(targetLayer, damage, impact, movePower, liveTime);            
+            twin.bSpawnTwin = false;
+
+            twin.MoveWave.waveInverse = !MoveWave.waveInverse;
+        }
+    }
+}
---
+++ Assets/Scripts/Projectiles/MoveWave.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWave : MonoBehaviour
+{
+    public float movePower = 1f;
+    Rigidbody2D rbody;
+
+    [HideInInspector] public bool waveInverse;
+    public float amplitude = 1f;
+    public float frequency = 1f;
+
+    Vector3 startPosition;
+    Quaternion rotation;
+
+    float ellapsedTime = 0;
+
+    BoundaryJump boundaryJump;
+    BoundaryJump BoundaryJump
+    {
+        get
+        {
+            if (!boundaryJump) boundaryJump = GetComponent<BoundaryJump>();
+            return boundaryJump;
+        }
+    }
+
+    private void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        rbody.velocity = Vector2.zero;
+
+        startPosition = transform.position;
+        rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 90);
+
+        if (BoundaryJump)
+        {
+            BoundaryJump.onJump.AddListener(delegate
+            {
+                ellapsedTime = 0;
+                startPosition = transform.position;
+                rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + 90);
+            });
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ellapsedTime += Time.fixedDeltaTime * movePower;
+        Move();
+    }
+
+    void Move()
+    {
+        float x = ellapsedTime;
+        float y = Mathf.Sin(ellapsedTime * frequency) * amplitude;
+        if (waveInverse) y *= -1;
+
+        Vector3 rotatedVector = rotation * new Vector3(x, y, 0);        
+
+        //transform.position = startPosition + rotatedVector;
+        rbody.MovePosition(startPosition + rotatedVector);
+    }
+}
