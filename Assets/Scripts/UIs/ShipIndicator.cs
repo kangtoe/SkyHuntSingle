633062589a52f9bb3ec454dd0f12659e2a0dcaec
kangtoe <kangtoe99@gmail.com>
2024-11-14 00:22:06 +0900

    플레이어 기체 강조 표시 UI 작성
    - 플레이어 기체가 화면 가장자리로 근처로 이동하는 경우에만 활성화 함
    - 가장자리에서 가시성이 떨어지는 것을 보완하기 위함

---
+++ Assets/Scripts/UIs/ShipIndicator.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteAlways]
+public class ShipIndicator : MonoBehaviour
+{
+    [SerializeField] float edgeCheckRange = 20;
+    [SerializeField] float indicateRange = 80;
+
+    Image image;
+    RectTransform rectTf;
+
+    private void Start()
+    {
+        image = GetComponent<Image>();
+        rectTf = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        Check();        
+    }
+
+    void Check()
+    {
+        Transform target = GameManager.Instance.PlayerShip.transform;
+        if (!target)
+        {
+            image.enabled = true;
+            return;
+        }
+
+        Vector3 screenPos = Camera.main.WorldToScreenPoint(target.position);
+        if (screenPos.x <= edgeCheckRange || screenPos.x >= Screen.width - edgeCheckRange || screenPos.y <= edgeCheckRange || screenPos.y >= Screen.height - edgeCheckRange)
+        {
+            image.enabled = true;
+
+            // pos
+            float x = Mathf.Clamp(screenPos.x, indicateRange, Screen.width - indicateRange);
+            float y = Mathf.Clamp(screenPos.y, indicateRange, Screen.height - indicateRange);
+            rectTf.position = new Vector2(x, y);
+            
+            // rot
+            Vector2 dir = Camera.main.WorldToScreenPoint(target.position) - rectTf.position;            
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            rectTf.rotation = Quaternion.Euler(0f, 0f, angle + 90);
+        }
+        else
+        {
+            image.enabled = false;
+        }
+    }
+}
